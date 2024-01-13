c588ac72c56072c7e1b95a2dd69924d802545b7c
Dackara <82636574+Dackara@users.noreply.github.com>
2024-01-13 11:21:40 +0100

    test pylontech

--- esphome/components/pylontech/pylontech.cpp
+++ esphome/components/pylontech/pylontech.cpp
@@ -11,6 +11,23 @@ static const uint8_t ASCII_LF = 0x0A;
 
 PylontechComponent::PylontechComponent() {}
 
+void PylontechComponent::setup() {
+  this->state_ = STATE_IDLE;
+  this->command_start_millis_ = 0;
+  ESP_LOGCONFIG(TAG, "Setting up pylontech...");
+//  while (this->available() != 0) {
+//    this->read();
+//  }
+}
+
+void PylontechComponent::empty_uart_buffer_() {
+  uint8_t byte;
+  while (this->available()) {
+    this->read_byte(&byte);
+  }
+}
+
+/*
 void PylontechComponent::dump_config() {
   this->check_uart_settings(115200, 1, esphome::uart::UART_CONFIG_PARITY_NONE, 8);
   ESP_LOGCONFIG(TAG, "pylontech:");
@@ -23,17 +40,297 @@ void PylontechComponent::dump_config() {
   }
 
   LOG_UPDATE_INTERVAL(this);
-}
+}*/
 
-void PylontechComponent::setup() {
-  ESP_LOGCONFIG(TAG, "Setting up pylontech...");
-  while (this->available() != 0) {
-    this->read();
+
+void PylontechComponent::loop() {
+  // Read message
+  if (this->state_ == STATE_IDLE) {
+    this->empty_uart_buffer_();
+    switch (this->send_next_command_()) {
+      case 0:
+        // no command send (empty queue) time to poll
+        if (millis() - this->last_poll_ > this->update_interval_) {
+          this->send_next_poll_();
+          this->last_poll_ = millis();
+        }
+        return;
+        break;
+      case 1:
+        // command send
+        return;
+        break;
+    }
+  }
+  if (this->state_ == STATE_COMMAND_COMPLETE) {
+    if (this->check_incoming_length_(4)) {
+      ESP_LOGD(TAG, "response length for command OK");
+      if (this->check_incoming_crc_()) {
+        // crc ok
+        if (this->read_buffer_[1] == 'A' && this->read_buffer_[2] == 'C' && this->read_buffer_[3] == 'K') {
+          ESP_LOGD(TAG, "command successful");
+        } else {
+          ESP_LOGD(TAG, "command not successful");
+        }
+        this->command_queue_[this->command_queue_position_] = std::string("");
+        this->command_queue_position_ = (command_queue_position_ + 1) % COMMAND_QUEUE_LENGTH;
+        this->state_ = STATE_IDLE;
+
+      } else {
+        // crc failed
+        this->command_queue_[this->command_queue_position_] = std::string("");
+        this->command_queue_position_ = (command_queue_position_ + 1) % COMMAND_QUEUE_LENGTH;
+        this->state_ = STATE_IDLE;
+      }
+    } else {
+      ESP_LOGD(TAG, "response length for command %s not OK: with length %zu",
+               this->command_queue_[this->command_queue_position_].c_str(), this->read_pos_);
+      this->command_queue_[this->command_queue_position_] = std::string("");
+      this->command_queue_position_ = (command_queue_position_ + 1) % COMMAND_QUEUE_LENGTH;
+      this->state_ = STATE_IDLE;
+    }
+  }
+
+  if (this->state_ == STATE_POLL_DECODED) {
+    std::string mode;
+    switch (this->used_polling_commands_[this->last_polling_command_].identifier) {
+      case POLLING_pwrsys:
+        if (this->systeme_is_) {
+          this->systeme_is_->publish_state(value_systeme_is_);
+        }
+        this->state_ = STATE_IDLE;
+        break;
+    }
+  }
+
+  if (this->state_ == STATE_POLL_CHECKED) {
+    bool enabled = true;
+    std::string fc;
+    char tmp[PYLONTECH_READ_BUFFER_LENGTH];
+    sprintf(tmp, "%s", this->read_buffer_);
+    switch (this->used_polling_commands_[this->last_polling_command_].identifier) {
+      case POLLING_pwrsys:
+        ESP_LOGD(TAG, "Decode pwrsys");
+        sscanf(tmp, "(System is %s", &l.value_systeme_is);                              // NOLINT
+        if (this->last_pwrsys_) {
+          this->last_pwrsys_->publish_state(tmp);
+        }
+        this->state_ = STATE_POLL_DECODED;
+        break;
+    }
+    return;
+  }
+  if (this->state_ == STATE_POLL_COMPLETE) {
+    if (this->check_incoming_crc_()) {
+      if (this->read_buffer_[0] == '(' && this->read_buffer_[1] == 'N' && this->read_buffer_[2] == 'A' &&
+          this->read_buffer_[3] == 'K') {
+        this->state_ = STATE_IDLE;
+        return;
+      }
+      // crc ok
+      this->state_ = STATE_POLL_CHECKED;
+      return;
+    } else {
+      this->state_ = STATE_IDLE;
+    }
+  }
+
+  if (this->state_ == STATE_COMMAND || this->state_ == STATE_POLL) {
+    while (this->available()) {
+      uint8_t byte;
+      this->read_byte(&byte);
+
+      if (this->read_pos_ == PYLONTECH_READ_BUFFER_LENGTH) {
+        this->read_pos_ = 0;
+        this->empty_uart_buffer_();
+      }
+      this->read_buffer_[this->read_pos_] = byte;
+      this->read_pos_++;
+
+      // end of answer
+      if (byte == 0x0D) {
+        this->read_buffer_[this->read_pos_] = 0;
+        this->empty_uart_buffer_();
+        if (this->state_ == STATE_POLL) {
+          this->state_ = STATE_POLL_COMPLETE;
+        }
+        if (this->state_ == STATE_COMMAND) {
+          this->state_ = STATE_COMMAND_COMPLETE;
+        }
+      }
+    }  // available
+  }
+
+  if (this->state_ == STATE_COMMAND) {
+    if (millis() - this->command_start_millis_ > esphome::pylontech::PylontechComponent::COMMAND_TIMEOUT) {
+      // command timeout
+      const char *command = this->command_queue_[this->command_queue_position_].c_str();
+      this->command_start_millis_ = millis();
+      ESP_LOGD(TAG, "timeout command from queue: %s", command);
+      this->command_queue_[this->command_queue_position_] = std::string("");
+      this->command_queue_position_ = (command_queue_position_ + 1) % COMMAND_QUEUE_LENGTH;
+      this->state_ = STATE_IDLE;
+      return;
+    } else {
+    }
+  }
+  if (this->state_ == STATE_POLL) {
+    if (millis() - this->command_start_millis_ > esphome::pylontech::PylontechComponent::COMMAND_TIMEOUT) {
+      // command timeout
+      ESP_LOGD(TAG, "timeout command to poll: %s", this->used_polling_commands_[this->last_polling_command_].command);
+      this->state_ = STATE_IDLE;
+    } else {
+    }
   }
 }
 
-void PylontechComponent::update() { this->write_str("pwr\n"); }
+uint8_t PylontechComponent::check_incoming_length_(uint8_t length) {
+  if (this->read_pos_ - 3 == length) {
+    return 1;
+  }
+  return 0;
+}
 
+uint8_t PylontechComponent::check_incoming_crc_() {
+  uint16_t crc16;
+  crc16 = this->pylontech_crc_(read_buffer_, read_pos_ - 3);
+  ESP_LOGD(TAG, "checking crc on incoming message");
+  if (((uint8_t) ((crc16) >> 8)) == read_buffer_[read_pos_ - 3] &&
+      ((uint8_t) ((crc16) &0xff)) == read_buffer_[read_pos_ - 2]) {
+    ESP_LOGD(TAG, "CRC OK");
+    read_buffer_[read_pos_ - 1] = 0;
+    read_buffer_[read_pos_ - 2] = 0;
+    read_buffer_[read_pos_ - 3] = 0;
+    return 1;
+  }
+  ESP_LOGD(TAG, "CRC NOK expected: %X %X but got: %X %X", ((uint8_t) ((crc16) >> 8)), ((uint8_t) ((crc16) &0xff)),
+           read_buffer_[read_pos_ - 3], read_buffer_[read_pos_ - 2]);
+  return 0;
+}
+
+// send next command used
+uint8_t PylontechComponent::send_next_command_() {
+  uint16_t crc16;
+  if (this->command_queue_[this->command_queue_position_].length() != 0) {
+    const char *command = this->command_queue_[this->command_queue_position_].c_str();
+    uint8_t byte_command[16];
+    uint8_t length = this->command_queue_[this->command_queue_position_].length();
+    for (uint8_t i = 0; i < length; i++) {
+      byte_command[i] = (uint8_t) this->command_queue_[this->command_queue_position_].at(i);
+    }
+    this->state_ = STATE_COMMAND;
+    this->command_start_millis_ = millis();
+    this->empty_uart_buffer_();
+    this->read_pos_ = 0;
+    crc16 = this->pylontech_crc_(byte_command, length);
+    this->write_str(command);
+    // checksum
+    this->write(((uint8_t) ((crc16) >> 8)));   // highbyte
+    this->write(((uint8_t) ((crc16) &0xff)));  // lowbyte
+    // end Byte
+    this->write(0x0D);
+    ESP_LOGD(TAG, "Sending command from queue: %s with length %d", command, length);
+    return 1;
+  }
+  return 0;
+}
+
+void PylontechComponent::send_next_poll_() {
+  uint16_t crc16;
+  this->last_polling_command_ = (this->last_polling_command_ + 1) % 15;
+  if (this->used_polling_commands_[this->last_polling_command_].length == 0) {
+    this->last_polling_command_ = 0;
+  }
+  if (this->used_polling_commands_[this->last_polling_command_].length == 0) {
+    // no command specified
+    return;
+  }
+  this->state_ = STATE_POLL;
+  this->command_start_millis_ = millis();
+  this->empty_uart_buffer_();
+  this->read_pos_ = 0;
+  crc16 = this->pylontech_crc_(this->used_polling_commands_[this->last_polling_command_].command,
+                               this->used_polling_commands_[this->last_polling_command_].length);
+  this->write_array(this->used_polling_commands_[this->last_polling_command_].command,
+                    this->used_polling_commands_[this->last_polling_command_].length);
+  // checksum
+  this->write(((uint8_t) ((crc16) >> 8)));   // highbyte
+  this->write(((uint8_t) ((crc16) &0xff)));  // lowbyte
+  // end Byte
+  this->write(0x0D);
+  ESP_LOGD(TAG, "Sending polling command : %s with length %d",
+           this->used_polling_commands_[this->last_polling_command_].command,
+           this->used_polling_commands_[this->last_polling_command_].length);
+}
+
+void PylontechComponent::queue_command_(const char *command, uint8_t length) {
+  uint8_t next_position = command_queue_position_;
+  for (uint8_t i = 0; i < COMMAND_QUEUE_LENGTH; i++) {
+    uint8_t testposition = (next_position + i) % COMMAND_QUEUE_LENGTH;
+    if (command_queue_[testposition].length() == 0) {
+      command_queue_[testposition] = command;
+      ESP_LOGD(TAG, "Command queued successfully: %s with length %u at position %d", command,
+               command_queue_[testposition].length(), testposition);
+      return;
+    }
+  }
+  ESP_LOGD(TAG, "Command queue full dropping command: %s", command);
+}
+
+void PylontechComponent::switch_command(const std::string &command) {
+  ESP_LOGD(TAG, "got command: %s", command.c_str());
+  queue_command_(command.c_str(), command.length());
+}
+void PylontechComponent::dump_config() {
+  ESP_LOGCONFIG(TAG, "Pylontech:");
+  ESP_LOGCONFIG(TAG, "used commands:");
+  for (auto &used_polling_command : this->used_polling_commands_) {
+    if (used_polling_command.length != 0) {
+      ESP_LOGCONFIG(TAG, "%s", used_polling_command.command);
+    }
+  }
+}
+
+void PylontechComponent::update() {} // { this->write_str("pwr\n"); }
+
+void PylontechComponent::add_polling_command_(const char *command, ENUMPollingCommand polling_command) {
+  for (auto &used_polling_command : this->used_polling_commands_) {
+    if (used_polling_command.length == strlen(command)) {
+      uint8_t len = strlen(command);
+      if (memcmp(used_polling_command.command, command, len) == 0) {
+        return;
+      }
+    }
+    if (used_polling_command.length == 0) {
+      size_t length = strlen(command) + 1;
+      const char *beg = command;
+      const char *end = command + length;
+      used_polling_command.command = new uint8_t[length];  // NOLINT(cppcoreguidelines-owning-memory)
+      size_t i = 0;
+      for (; beg != end; ++beg, ++i) {
+        used_polling_command.command[i] = (uint8_t) (*beg);
+      }
+      used_polling_command.errors = 0;
+      used_polling_command.identifier = polling_command;
+      used_polling_command.length = length - 1;
+      return;
+    }
+  }
+}
+
+uint16_t PylontechComponent::pylontech_crc_(uint8_t *msg, uint8_t len) {
+  uint16_t crc = crc16be(msg, len);
+  uint8_t crc_low = crc & 0xff;
+  uint8_t crc_high = crc >> 8;
+  if (crc_low == 0x28 || crc_low == 0x0d || crc_low == 0x0a)
+    crc_low++;
+  if (crc_high == 0x28 || crc_high == 0x0d || crc_high == 0x0a)
+    crc_high++;
+  crc = (crc_high << 8) | crc_low;
+  return crc;
+}
+
+/*
 void PylontechComponent::loop() {
   if (this->available() > 0) {
     // pylontech sends a lot of data very suddenly
@@ -60,7 +357,8 @@ void PylontechComponent::loop() {
       buffer_index_read_ = (buffer_index_read_ + 1) % NUM_BUFFERS;
     }
   }
-}
+}*/
+
 
 void PylontechComponent::process_line_(std::string &buffer) {
   ESP_LOGV(TAG, "Read from serial: %s", buffer.substr(0, buffer.size() - 2).c_str());
--- esphome/components/pylontech/pylontech.h
+++ esphome/components/pylontech/pylontech.h
@@ -1,14 +1,118 @@
 #pragma once
 
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/uart/uart.h"
 #include "esphome/core/component.h"
 #include "esphome/core/defines.h"
-#include "esphome/components/uart/uart.h"
 
 namespace esphome {
 namespace pylontech {
 
+enum ENUMPollingCommand {
+  POLLING_pwrsys = 0,
+  POLLING_pwr = 1,
+  POLLING_stat = 2,
+  POLLING_getpwr = 3,
+  POLLING_bat = 4,
+  POLLING_info = 5,
+};
+struct PollingCommand {
+  uint8_t *command;
+  uint8_t length = 0;
+  uint8_t errors;
+  ENUMPollingCommand identifier;
+};
+
+#define PYLONTECH_VALUED_ENTITY_(type, name, polling_command, value_type) \
+ protected: \
+  value_type value_##name##_; \
+  PYLONTECH_ENTITY_(type, name, polling_command)
+
+#define PYLONTECH_ENTITY_(type, name, polling_command) \
+ protected: \
+  type *name##_{}; /* NOLINT */ \
+\
+ public: \
+  void set_##name(type *name) { /* NOLINT */ \
+    this->name##_ = name; \
+    this->add_polling_command_(#polling_command, POLLING_##polling_command); \
+  }
+
+#define PYLONTECH_SENSOR(name, polling_command, value_type) \
+  PYLONTECH_VALUED_ENTITY_(sensor::Sensor, name, polling_command, value_type)
+
+#define PYLONTECH_VALUED_TEXT_SENSOR(name, polling_command, value_type) \
+  PYLONTECH_VALUED_ENTITY_(text_sensor::TextSensor, name, polling_command, value_type)
+#define PYLONTECH_TEXT_SENSOR(name, polling_command) PYLONTECH_ENTITY_(text_sensor::TextSensor, name, polling_command)
+
 static const uint8_t NUM_BUFFERS = 20;
 static const uint8_t TEXT_SENSOR_MAX_LEN = 8;
+
+class PylontechComponent : public PollingComponent, public uart::UARTDevice {
+  
+  PYLONTECH_SENSOR(systeme_is, pwrsys, float)
+  
+ public:
+  PylontechComponent();
+
+  void switch_command(const std::string &command);
+  /// Setup the sensor and test for a connection.
+  void setup() override;
+  /// Read data once available
+  void loop() override;
+  void dump_config() override;
+  /// Schedule data readings.
+  void update() override;
+
+  float get_setup_priority() const override; //possible delete
+
+  void register_listener(PylontechListener *listener) { this->listeners_.push_back(listener); }
+
+ protected:
+  void process_line_(std::string &buffer);
+
+  // ring buffer
+  std::string buffer_[NUM_BUFFERS];
+  int buffer_index_write_ = 0;
+  int buffer_index_read_ = 0;
+
+  std::vector<PylontechListener *> listeners_{};
+
+  //Pipsolar Import
+  static const size_t PYLONTECH_READ_BUFFER_LENGTH = 110;  // maximum supported answer length
+  static const size_t COMMAND_QUEUE_LENGTH = 10;
+  static const size_t COMMAND_TIMEOUT = 5000;
+  uint32_t last_poll_ = 0;
+  void add_polling_command_(const char *command, ENUMPollingCommand polling_command);
+  void empty_uart_buffer_();
+  uint8_t check_incoming_crc_();
+  uint8_t check_incoming_length_(uint8_t length);
+  uint16_t pylontech_crc_(uint8_t *msg, uint8_t len); //utile ?
+  uint8_t send_next_command_();
+  void send_next_poll_();
+  void queue_command_(const char *command, uint8_t length);
+  std::string command_queue_[COMMAND_QUEUE_LENGTH];
+  uint8_t command_queue_position_ = 0;
+  uint8_t read_buffer_[PYLONTECH_READ_BUFFER_LENGTH];
+  size_t read_pos_{0};
+
+  uint32_t command_start_millis_ = 0;
+  uint8_t state_;
+  enum State {
+    STATE_IDLE = 0,
+    STATE_POLL = 1,
+    STATE_COMMAND = 2,
+    STATE_POLL_COMPLETE = 3,
+    STATE_COMMAND_COMPLETE = 4,
+    STATE_POLL_CHECKED = 5,
+    STATE_POLL_DECODED = 6,
+  };
+
+  uint8_t last_polling_command_ = 0;
+  PollingCommand used_polling_commands_[15];
+};
+
 
 class PylontechListener {
  public:
@@ -22,32 +126,6 @@ class PylontechListener {
   virtual void dump_config();
 };
 
-class PylontechComponent : public PollingComponent, public uart::UARTDevice {
- public:
-  PylontechComponent();
-
-  /// Schedule data readings.
-  void update() override;
-  /// Read data once available
-  void loop() override;
-  /// Setup the sensor and test for a connection.
-  void setup() override;
-  void dump_config() override;
-
-  float get_setup_priority() const override;
-
-  void register_listener(PylontechListener *listener) { this->listeners_.push_back(listener); }
-
- protected:
-  void process_line_(std::string &buffer);
-
-  // ring buffer
-  std::string buffer_[NUM_BUFFERS];
-  int buffer_index_write_ = 0;
-  int buffer_index_read_ = 0;
-
-  std::vector<PylontechListener *> listeners_{};
-};
 
 }  // namespace pylontech
 }  // namespace esphome
